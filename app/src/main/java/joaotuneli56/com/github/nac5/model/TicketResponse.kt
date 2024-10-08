85430a8e89fdd42e7fcee423b123ed47ec9863ba
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-07 22:34:36 -0300

    first commit

---
+++ app/src/main/java/joaotuneli56/com/github/nac5/model/TicketResponse.kt
@@ -0,0 +1,14 @@
+package joaotuneli56.com.github.nac5.model
+
+class TickerResponse(
+    val ticker: Ticker
+)
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
